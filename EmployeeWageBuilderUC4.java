96d47e26b0026be88029ebfd1ad058641716f441
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 11:26:59 +0530

    ADDED:Calculated employee wage based on their shift using switch case.

---
+++ EmployeeWageBuilderUC4.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class EmployeeWageBuilderUC4 {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+        //Variables
+        int empHours = 0;
+        int empWage = 0;
+        //Computation
+        int empCheck = (int) Math.floor(Math.random () * 10)%3;
+        switch (empCheck){
+            case IS_FULL_TIME:
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                empHours = 4;
+                break;
+            default:
+                empHours = 0;
+        }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("Employee wage is " +empWage);
+
+    }
+}
