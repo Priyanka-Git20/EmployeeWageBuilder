8ba71bbb73f6450acabdf671ac2e213954da6f49
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 11:09:53 +0530

    ADDED:Welcome message on the master branch.

---
+++ EmployeeWageBuilderUC.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeeWageBuilderUC {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the employee wage computation program.");
+
+    }
+}
